938970f418b8d4756e0aa04e804f1be3556ed3bd
Przemek Saja <przemek.saja@gmail.com>
2018-07-01 19:49:06 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+//zadanie 13.7
+
+var fs = require('fs');
+//var StatMode = require('stat-mode');
+//var colors = require('colors');
+
+/*fs.stat('./cat.jpg', function(err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+});
+*/
+
+fs.readdir('./folder', 'utf-8', function(err, files) {
+	if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
+    console.log('Katalog folder zawiera:');
+    console.log(files);
+	fs.writeFile('./lista_plikow.txt', files , function(err) {
+    	if (err) throw err; 
+    	console.log('Zapisano zawartość folderu w pliku lista_plików');
+	});        
+});
